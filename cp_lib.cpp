d078e4781ec43525d6d2060acf6ab36e9f099932
Soumik Kumar Basu <97104371+soumikiith@users.noreply.github.com>
2024-01-25 20:03:40 +0530

    Create cp_lib.cpp

---
+++ cp_lib.cpp
@@ -0,0 +1,24 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+
+using namespace llvm;
+
+namespace {
+struct constant_p : public FunctionPass {
+  static char ID;
+  Hello() : FunctionPass(ID) {}
+
+  bool runOnFunction(Function &F) override {
+    // write your code here
+    return false;
+  }
+}; // end of struct constant_p
+}  // end of anonymous namespace
+
+char constant_p::ID = 0;
+static RegisterPass<Hello> X("libCP_given", "Constant Propagation Pass for Assignment",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
